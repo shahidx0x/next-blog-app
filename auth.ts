d4339d3e7ea414c61ef8515a97994fab57b81a13
shahidx0x <dev.imshahid@gmail.com>
2024-03-10 05:23:39 +0600

    fix : auth bug fixed

--- auth.ts
+++ auth.ts
@@ -25,37 +25,47 @@ export const authOptions: NextAuthOptions = {
   },
 
   providers: [
-    // Credentials({
-    //   name: "Credentials",
-    //   credentials: {
-    //     email: { label: "Email", type: "email", placeholder: "email" },
-    //     password: { label: "Password", type: "password" },
-    //   },
-    //   async authorize(credentials, req): Promise<any> {
-    //     const { email, password } = credentials as {
-    //       email: string;
-    //       password: string;
-    //     };
-    //     const response = await userService.authenticate(email, password);
-    //     let user = {
-    //       id: response.data.user.id,
-    //       name: response.data.user.name,
-    //       image: response.data.user.image,
-    //       email: response.data.user.email,
-    //     };
-    //     return user;
-    //   },
-    // }),
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req): Promise<any> {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        const response = await userService.authenticate(email, password);
+        let user = {
+          id: response.data.user.id,
+          name: response.data.user.name,
+          image: response.data.user.image,
+          email: response.data.user.email,
+        };
+        return user;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      // authorization: {
-      //   params: {
-      //     prompt: "consent",
-      //     access_type: "offline",
-      //     response_type: "code",
-      //   },
-      // },
+      profile(profile): any {
+        return {
+          id: profile.sub,
+          name: `${profile.given_name} ${profile.family_name}`,
+          email: profile.email,
+          image: profile.picture,
+          role: profile.role ? profile.role : "user",
+        };
+      },
+
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
 };
